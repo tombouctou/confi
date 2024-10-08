3cc88f77512c7cdf619a5b0453795c6da8611da6
Egor Tarasov <pavluchenk0@yandex.ru>
2024-10-08 16:55:02 +0400

    confi usage updates (#3)

--- dotnet/core/Core.cs
+++ dotnet/core/Core.cs
@@ -1,25 +1,22 @@
-﻿using System.Reflection;
-using Confi;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-public static class Extensions 
+namespace Confi;
+
+public static class ConfigurationExtensions 
 {
     public static string GetRequiredValue(this IConfiguration configuration, string key)
     {
         return configuration[key] ?? throw new MissingConfigurationKeyException(key);
     }
 
-    public static string GetRequiredConfigurationValue(IServiceProvider serviceProvider, string configurationKey)
+    public static string GetRequiredConfigurationValue(this IServiceProvider serviceProvider, string configurationKey)
     {
         var configuration = serviceProvider.GetRequiredService<IConfiguration>();
         return configuration.GetRequiredValue(configurationKey);
     }
 }
 
-namespace Confi
+public class MissingConfigurationKeyException(string key) : Exception(message: $"Unable to find configuration value by key `{key}`")
 {
-    public class MissingConfigurationKeyException(string key) : Exception(message: $"Unable to find configuration value by key `{key}`")
-    {
-    }
 }
